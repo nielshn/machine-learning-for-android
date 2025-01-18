e741d9dfbc177c46d3b0cdda273b4ce04951f420
nielshn <niel.shn08@gmail.com>
2025-01-18 11:44:31 +0700

    practicing MediaPipe module

---
+++ MediaPipe/TextClassification/app/src/main/java/com/dicoding/textclassification/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.dicoding.textclassification
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.textclassification.databinding.ActivityMainBinding
+import com.google.mediapipe.tasks.components.containers.Classifications
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val textClassifierHelper = TextClassifierHelper(
+            context = this,
+            classifierListener = object : TextClassifierHelper.ClassifierListener {
+                override fun onError(error: String) {
+                    Toast.makeText(this@MainActivity, error, Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResults(results: List<Classifications>?, inferenceTime: Long) {
+                    runOnUiThread {
+                        results?.let { it ->
+                            if (it.isNotEmpty() && it[0].categories().isNotEmpty()) {
+                                println(it)
+                                val sortedCategories =
+                                    it[0].categories().sortedByDescending { it?.score() }
+
+                                val displayResult =
+                                    sortedCategories.joinToString("\n") {
+                                        "${it.categoryName()} " + NumberFormat.getPercentInstance()
+                                            .format(it.score()).trim()
+                                    }
+                                binding.tvResult.text = displayResult
+                            } else {
+                                binding.tvResult.text = ""
+                            }
+                        }
+                    }
+                }
+            }
+        )
+
+        binding.btnClassify.setOnClickListener {
+            val inputText = binding.edInput.text.toString()
+            textClassifierHelper.classify(inputText)
+        }
+    }
+}
